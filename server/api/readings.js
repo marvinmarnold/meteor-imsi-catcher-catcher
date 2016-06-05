8e56188b69bee15e9ac2eb1e85dee11a0af7f003
Marvin Arnold <marvinmarnold@gmail.com>
2016-06-05 18:29:29 -0500

    Do not attempt to insert malformed readings

--- server/api/readings.js
+++ server/api/readings.js
@@ -8,29 +8,32 @@ import {
 
 Meteor.methods({
   // Return the result of Reading.insert or false if duplicate entry
-  'catcher.readings.insert': function(reading) {
-    console.log('catcher.readings.insert');
-    CommonReadingsSchema.namedContext().validate(reading.commonReading);
+  'catcher.readings.insert': (reading) => {
+    // console.log('catcher.readings.insert');
+    // console.log(reading);
+    const validCommonReading = CommonReadingsSchema.namedContext().validate(reading.commonReading);
 
-    console.log('passed first validation');
-    // Must register deviceId before can add a reading
-    // if(!DeviceId.isClaimed(reading.commonReading.deviceId))
-    //   throw new Meteor.Error('invalid-device-id', "DeviceId has not been registered.");
+    if(validCommonReading) {
+      // Must register deviceId before can add a reading
+      // if(!DeviceId.isClaimed(reading.commonReading.deviceId))
+      //   throw new Meteor.Error('invalid-device-id', "DeviceId has not been registered.");
 
 
-    // Ensure reading is formatted correctly
-    var readingCollection =   collectionForReadingType(reading.commonReading.readingType);
-    readingCollection.simpleSchema().namedContext().validate(reading);
+      // Ensure reading is formatted correctly
+      var readingCollection = collectionForReadingType(reading.commonReading.readingType);
+      const validReading = readingCollection.simpleSchema().namedContext().validate(reading);
 
-    if(!isDuplicateReading(reading)) {
-      return readingCollection.insert(reading);
-    } else {
-      return false
+      if(validReading && !isDuplicateReading(reading)) {
+        return readingCollection.insert(reading);
+      } else {
+        return false
+      }
     }
+
   }
 });
 
-Meteor.publish("catcher.readings.device", function(deviceId) {
+Meteor.publish("catcher.readings.device", deviceId => {
   check(deviceId, String);
 
   return [
